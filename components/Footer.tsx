e474507c9ff15c8f2225fdc28840436a1b12e90a
sandyddeveloper <sandyddeveloper@gmail.com>
2025-03-15 17:41:36 +0530

    footer iupdated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-
 import Image from "next/image";
 import Link from "next/link";
 import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
-
-
   return (
     <div className="py-8 px-6 md:px-16 bg-[#0D0D2B] text-gray-400 border-t border-gray-700 mt-10">
-      <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
-     
+      <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0">
+        {/* Logo */}
         <Link href="/" className="flex items-center space-x-3">
           <Image
-            src="/Logo.png" 
+            src="/Logo.png"
             alt="StackScripts Logo"
             width={40}
             height={40}
@@ -24,41 +21,28 @@ export default function Footer() {
             Stack<span className="text-blue-500">Scripts</span>
           </span>
         </Link>
-        <div className="flex space-x-6 text-sm text-gray-400 z-50">
-        <Link
-  href="/terms"
-  className="hover:text-blue-400 transition duration-300"
->
-  Terms of Use
-</Link>
-<Link
-  href="/privacy-policy"
-  className="hover:text-blue-500 transition duration-300"
->
-  Privacy Policy
-</Link>
-<Link
-  href="/cookie-policy"
-  className="hover:text-blue-500 transition duration-300"
->
-  Cookie Policy
-</Link>
-<Link
-  href="/about-us"
-  className="hover:text-blue-500 transition duration-300"
->
-  About Us
-</Link>
-<Link
-  href="/contact"
-  className="hover:text-blue-500 transition duration-300"
->
-  Contact
-</Link>
 
+        {/* Policy Links */}
+        <div className="flex flex-wrap justify-center gap-4 text-sm text-gray-400 z-50">
+          <Link href="/terms" className="hover:text-blue-400 transition duration-300">
+            Terms of Use
+          </Link>
+          <Link href="/privacy-policy" className="hover:text-blue-500 transition duration-300">
+            Privacy Policy
+          </Link>
+          <Link href="/cookie-policy" className="hover:text-blue-500 transition duration-300">
+            Cookie Policy
+          </Link>
+          <Link href="/about-us" className="hover:text-blue-500 transition duration-300">
+            About Us
+          </Link>
+          <Link href="/contact" className="hover:text-blue-500 transition duration-300">
+            Contact
+          </Link>
         </div>
 
-        <div className="flex space-x-5 z-50">
+        {/* Social Icons */}
+        <div className="flex space-x-6 md:space-x-5 z-50">
           <Link href="https://twitter.com/yourprofile" target="_blank">
             <FaTwitter className="w-6 h-6 text-gray-400 hover:text-blue-500 transition" />
           </Link>
@@ -72,7 +56,7 @@ export default function Footer() {
       </div>
 
       {/* Centered Copyright */}
-      <div className="text-center text-sm text-gray-500 mt-4">
+      <div className="text-center text-sm text-gray-500 mt-6">
         © 2025 MetaBlog. All rights reserved.
       </div>
     </div>
